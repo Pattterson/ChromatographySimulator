55efcc8b5ddfdaa694f4a0da0bd1373761846d18
alexpatterson <alexpattterson@gmail.com>
2019-01-17 19:31:50 -0600

    Improved noise generation formula

--- src/ChromatographyHome/NoiseGenerator.java
+++ src/ChromatographyHome/NoiseGenerator.java
@@ -1,5 +1,6 @@
 package ChromatographyHome;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class NoiseGenerator {
@@ -9,8 +10,11 @@ public class NoiseGenerator {
     static int generateNoise(){
         int min = 20;
         int max = 25;
+        Random rand = new Random();
 
-        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+
+
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1) + (int) (5*rand.nextDouble());;
 
         return randomNum;
 
