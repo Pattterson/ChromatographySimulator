a0af8377068e465a4ec5fab49ab486da383350db
alexpatterson <alexpattterson@gmail.com>
2019-01-27 12:46:24 -0600

    Maven support added in order to allow cdk library and mongodb driver to be used in future

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-//module ChromatographySimulator {
-//    requires javafx.fxml;
-//    requires javafx.controls;
-//    requires javafx.media;
-//    requires javafx.web;
-//
-//    opens ChromatographyHome;
-//}
