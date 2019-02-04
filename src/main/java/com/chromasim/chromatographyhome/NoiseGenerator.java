7dc7fec321b98fc73cec96d9ef72ea73bbbacf45
alexpatterson <alexpattterson@gmail.com>
2019-02-03 19:48:17 -0600

    Noise generator improved to better mimic real chromatographic noise

--- src/main/java/com/chromasim/chromatographyhome/NoiseGenerator.java
+++ src/main/java/com/chromasim/chromatographyhome/NoiseGenerator.java
@@ -5,18 +5,12 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class NoiseGenerator {
     //No need to instantiate;
-    private NoiseGenerator(){};
+    private NoiseGenerator(){}
 
-    static int generateNoise(){
-        int min = 20;
-        int max = 25;
+    static double generateNoise(){
         Random rand = new Random();
 
-
-
-        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1) + (int) (5*rand.nextDouble());;
-
-        return randomNum;
+        return rand.nextGaussian()*2+5;
 
     }
 
